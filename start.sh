8d321343e873800d5b1b90adba79eb44d67dfded
Nik <n.v.pushkin@gmail.com>
2024-11-16 19:20:02 +0400

    new starting flow

--- start.sh
+++ start.sh
@@ -5,6 +5,9 @@ echo "Worker Initiated"
 # echo "Symlinking files from Network Volume"
 # rm -rf /workspace && \
 #   ln -s /runpod-volume /workspace
+if [ ! -d /workspace ]; then
+    ln -s /runpod-volume /workspace
+fi
 
 if [ -f "/workspace/venv/bin/activate" ]; then
     echo "Starting WebUI API"
