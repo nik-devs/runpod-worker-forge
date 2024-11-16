4c0e1a79642b0a88754030685f0e3a3c38fdc57d
nik-devs <n.v.pushkin@gmail.com>
2024-11-16 13:27:22 +0400

    Update start.sh - change github link

--- start.sh
+++ start.sh
@@ -31,7 +31,7 @@ if [ -f "/workspace/venv/bin/activate" ]; then
     deactivate
 else
     echo "ERROR: The Python Virtual Environment (/workspace/venv/bin/activate) could not be activated"
-    echo "1. Ensure that you have followed the instructions at: https://github.com/ashleykleynhans/runpod-worker-a1111/blob/main/docs/installing.md"
+    echo "1. Ensure that you have followed the instructions at: https://github.com/nik-devs/runpod-worker-a1111/blob/main/docs/installing.md"
     echo "2. Ensure that you have used the Pytorch image for the installation and NOT a Stable Diffusion image."
     echo "3. Ensure that you have attached your Network Volume to your endpoint."
     echo "4. Ensure that you didn't assign any other invalid regions to your endpoint."
